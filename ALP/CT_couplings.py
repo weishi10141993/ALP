0aa99a67627b7a0946b9b0dad089b54973726729
Wei Shi <weishi@rice.edu>
2018-07-11 19:47:03 +0200

    Add files via upload

---
+++ ALP/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 11.0.0 for Mac OS X x86 (64-bit) (July 28, 2016)
+# Date: Mon 13 Nov 2017 11:32:38
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ ALP/coupling_orders.py
@@ -0,0 +1,28 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 11.0.0 for Mac OS X x86 (64-bit) (July 28, 2016)
+# Date: Mon 13 Nov 2017 11:32:37
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 2)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HIG = CouplingOrder(name = 'HIG',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+HIW = CouplingOrder(name = 'HIW',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
